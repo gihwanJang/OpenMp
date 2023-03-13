6ab2d20525a1ead25c4310c2d8f9a7ea196e823a
gihwan <gihwan319@gmail.com>
2023-03-13 21:09:39 +0900

    HelloOpenMP

---
+++ HelloOpenMP/src/HelloOpenMP.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char* argv[]){
+	int thread_count = strtol(argv[1], NULL, 10);
+
+	#pragma omp parallel num_threads(thread_count)
+	printf("[Thread %d] Hello OpenMP!\n", omp_get_thread_num());
+
+	return 0;
+}
